4ae81717a6794ce737f7e73c38bb22e444cc3756
dnakano <dnakano@student.42tokyo.jp>
2021-02-15 08:18:31 +0900

    Update: map: add destructors and other function in progress
    ### done
    - destructor
    - destructNodes_ member function
    - cloneVal_ member function
    
    ### in progress
    - insert
    - operator[]

--- map.hpp
+++ map.hpp
@@ -6,20 +6,43 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 13:39:34 by dnakano           #+#    #+#             */
-/*   Updated: 2021/02/14 16:05:29 by dnakano          ###   ########.fr       */
+/*   Updated: 2021/02/15 07:59:49 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAP_HPP
 #define MAP_HPP
 
+#include "iterator.hpp"
+
 namespace ft {
 
-// template <class Pointer>
-// struct TreeNode {
-//   Pointer value_;
-//   TreeNode
-// };
+template <class Pointer>
+struct TreeNode {
+  Pointer value_;
+  TreeNode* left_;
+  TreeNode* right_;
+
+  TreeNode() {
+    value_ = NULL;
+    left_ = NULL;
+    right_ = NULL;
+  }
+
+  TreeNode(Pointer value) {
+    value_ = value;
+    left_ = NULL;
+    right_ = NULL;
+  }
+
+  TreeNode(Pointer value, TreeNode* left, TreeNode* right) {
+    value_ = value;
+    left_ = left;
+    right_ = right;
+  }
+
+  virtual ~TreeNode() {}
+};
 
 template <class Key, class T, class Compare = std::less<Key>,
           class Allocator = std::allocator<std::pair<const Key, T> > >
@@ -52,21 +75,103 @@ class map {
   };
 
   /*** private class definitions ***/
+  typedef TreeNode<pointer> node_type;
+  typedef node_type* node_pointer;
 
-  // typedef Node_ node_type;
-  // typedef *node_type node_pointer;
+  /*** iterators ***/
+  typedef bidirectional_iterator_tree_<value_type, node_pointer,
+                                       difference_type, pointer, reference>
+      iterator;
+  typedef bidirectional_iterator_tree_<const value_type, node_pointer,
+                                       difference_type, pointer, reference>
+      const_iterator;
+  typedef ft::reverse_iterator<iterator> reverse_iterator;
+  typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
 
   /*** private members ***/
  private:
   key_compare comp_;
   allocator_type alloc_;
+  node_pointer root_;
+  size_type depth_;
+
+  /*** private member functions ***/
+  pointer cloneVal_(const value_type& val) {
+    pointer val_ptr = alloc_.allocate(1);
+    alloc_.construct(val_ptr, val);
+    return val_ptr;
+  }
+
+  void destructNodes_(node_pointer node) {
+    if (node == NULL) {
+      return ;
+    }
+    alloc_.destroy(node->value_);
+    alloc_.deallocate(node->value_, 1);
+    destructNodes_(node->left_);
+    destructNodes_(node->right_);
+    delete node;
+  }
 
   /*** constructors ***/
  public:
   explicit map(const key_compare& comp = key_compare(),
                const allocator_type& alloc = allocator_type()) {
+    root_ = NULL;
     comp_ = comp;
     alloc_ = alloc;
+    depth_ = 0;
+  }
+
+  ~map() {
+    destructNodes_(root_);
+  }
+
+  /*** operator overloads ***/
+  mapped_type& operator[](const key_type& key) {
+    (void)key;
+    return (*root_->value_).second;
+  }
+
+  /*** modifiers ***/
+  std::pair<iterator, bool> insert(const value_type& val) {
+    if (root_ == NULL) {  // also means depth == 0
+      root_ = new node_type(cloneVal_(val));
+      std::cout << "inserted: " << (*root_->value_).first << ": " << (*root_->value_).second << std::endl;
+      return std::pair<iterator, bool>(iterator(root_, root_), true);
+    }
+
+    node_pointer node = root_;
+    size_type depth = 1;
+    while (1) {
+      if (comp_(val.first, (*node->value_).first)) {
+        ++depth;
+        // case val.key < node->val.key
+        if (node->left_ == NULL) {
+          node->left_ = new node_type(cloneVal_(val));
+          break;
+        } else {
+          node = node->left_;
+        }
+      } else if (comp_((*node->value_).first, val.first)) {
+        // case val.key > node->val.key
+        if (node->right_ == NULL) {
+          node->right_ = new node_type(cloneVal_(val));
+          break;
+        } else {
+          node = node->right_;
+        }
+      } else {
+        // case val.key already exists
+        return std::pair<iterator, bool>(iterator(node, root_), false);
+      }
+    }
+    if (depth > depth_ /*+ 1*/) {
+      // kaiten
+      depth_ = depth;
+    }
+    std::cout << "inserted: " << (*node->value_).first << ": " << (*node->value_).second << std::endl;
+    return std::pair<iterator, bool>(iterator(node, root_), true);
   }
 };
 
--- test/test_map.cpp
+++ test/test_map.cpp
@@ -6,7 +6,7 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 13:37:31 by dnakano           #+#    #+#             */
-/*   Updated: 2021/02/14 15:31:32 by dnakano          ###   ########.fr       */
+/*   Updated: 2021/02/15 07:51:26 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,18 @@ void test_map(int& test_no) {
       std::pair<int, std::string>(6, "foobar"),
       std::pair<int, std::string>(3, "norminette")};
 
+  std::pair<Hoge, Hoge> hoges[10] = {
+      std::pair<Hoge, Hoge>(Hoge(0, 7), Hoge(0, 0)),
+      std::pair<Hoge, Hoge>(Hoge(1, 2), Hoge(1, 1)),
+      std::pair<Hoge, Hoge>(Hoge(2, 4), Hoge(2, 2)),
+      std::pair<Hoge, Hoge>(Hoge(3, 0), Hoge(3, 3)),
+      std::pair<Hoge, Hoge>(Hoge(4, 9), Hoge(4, 4)),
+      std::pair<Hoge, Hoge>(Hoge(5, 8), Hoge(5, 5)),
+      std::pair<Hoge, Hoge>(Hoge(6, 5), Hoge(6, 6)),
+      std::pair<Hoge, Hoge>(Hoge(7, 1), Hoge(7, 7)),
+      std::pair<Hoge, Hoge>(Hoge(8, 6), Hoge(8, 8)),
+      std::pair<Hoge, Hoge>(Hoge(9, 3), Hoge(9, 9))};
+
   putTestInfo(test_no, "map<int, string>: default construction");
   try {
     std::map<int, std::string> std_map;
@@ -45,7 +57,44 @@ void test_map(int& test_no) {
   }
   std::cout << " => OK :)" << std::endl;
 
-  putTestInfo(test_no, "map<int, string>: default construction");
+  putTestInfo(test_no, "map<int, string>: insert to map(0)");
+  try {
+    std::map<int, std::string> std_map;
+    ft::map<int, std::string> ft_map;
+
+    std_map.insert(persons[0]);
+    ft_map.insert(persons[0]);
+    if (std_map[7] != ft_map[7]) {
+      throw std::runtime_error("value");
+    }
+  } catch (std::exception& e) {
+    throw std::runtime_error(e.what());
+  }
+  std::cout << " => OK :)" << std::endl;
+
+  putTestInfo(test_no, "map<int, string>: insert 2 to map(0)");
+  try {
+    std::map<int, std::string> std_map;
+    ft::map<int, std::string> ft_map;
+
+    std_map.insert(persons[0]);
+    ft_map.insert(persons[0]);
+    std_map.insert(persons[1]);
+    ft_map.insert(persons[1]);
+    if (std_map[7] != ft_map[7]) {
+      throw std::runtime_error("value");
+    }
+    if (std_map[2] != ft_map[2]) {
+      throw std::runtime_error("value");
+    }
+  } catch (std::exception& e) {
+    throw std::runtime_error(e.what());
+  }
+  std::cout << " => OK :)" << std::endl;
+
+  return ;
+
+  putTestInfo(test_no, "map<int, string>: fill construction(10)");
   try {
     std::map<int, std::string> std_map(persons, persons + 10);
     std::map<int, std::string>::iterator std_itr;
@@ -56,7 +105,38 @@ void test_map(int& test_no) {
                 << std::endl;
     }
   } catch (std::exception& e) {
-    throw std::runtime_error("failed");
+    throw std::runtime_error(e.what());
+  }
+  std::cout << " => OK :)" << std::endl;
+
+  putTestInfo(test_no, "map<Hoge, Hoge>: fill construction(10)");
+  try {
+    std::map<Hoge, Hoge> std_map(hoges, hoges + 10);
+    std::map<Hoge, Hoge>::iterator std_itr;
+
+    std::cout << std::endl;
+    for (std_itr = std_map.begin(); std_itr != std_map.end(); ++std_itr) {
+      std::cout << "map[" << (*std_itr).first << "] = " << (*std_itr).second
+                << std::endl;
+    }
+  } catch (std::exception& e) {
+    throw std::runtime_error(e.what());
+  }
+  std::cout << " => OK :)" << std::endl;
+
+  putTestInfo(test_no, "map<Hoge, Hoge>: fill construction(10)");
+  try {
+    std::map<Hoge, Hoge> std_map(hoges, hoges + 10);
+    std::map<Hoge, Hoge>::iterator std_itr;
+
+    std_map[Hoge(1, 1)] = Hoge(-1, -1);
+    std::cout << std::endl;
+    for (std_itr = std_map.begin(); std_itr != std_map.end(); ++std_itr) {
+      std::cout << "map[" << (*std_itr).first << "] = " << (*std_itr).second
+                << std::endl;
+    }
+  } catch (std::exception& e) {
+    throw std::runtime_error(e.what());
   }
   std::cout << " => OK :)" << std::endl;
 
