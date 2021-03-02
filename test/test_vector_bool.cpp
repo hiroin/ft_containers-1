944f003f66be4f2387e09e381ffee7285fe7311c
dnakano <dnakano@student.42tokyo.jp>
2021-03-02 20:05:15 +0900

    Add: vector<bool>: erase(position)

--- test/test_vector_bool.cpp
+++ test/test_vector_bool.cpp
@@ -6,7 +6,7 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 08:32:48 by dnakano           #+#    #+#             */
-/*   Updated: 2021/03/02 19:10:26 by dnakano          ###   ########.fr       */
+/*   Updated: 2021/03/02 20:03:09 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -1167,6 +1167,73 @@ void test_vector_bool(int& test_no) {
         }
         std::cout << " => OK :)" << std::endl;
       }
+    }
+  }
+
+  for (size_t i = 1; i < 15; i++) {
+    for (size_t j = 0; j < i; j++) {
+      size = size_ary[i];
+      std::stringstream sout;
+      sout << "Vector<bool>: erase(begin + " << size_ary[j] << ") from vec("
+           << size << ")";
+
+      putTestInfo(test_no, sout.str());
+      try {
+        bool val[size];
+        for (size_t idx = 0; idx < size; ++idx) {
+          val[idx] = rand() % 2;
+        }
+        std::vector<bool> std_vec(val, val + size);
+        ft::vector<bool> ft_vec(val, val + size);
+        std::vector<bool>::iterator std_itr = std_vec.begin();
+        ft::vector<bool>::iterator ft_itr = ft_vec.begin();
+        for (size_t idx = 0; idx < size_ary[j]; ++idx) {
+          ++std_itr;
+          ++ft_itr;
+        }
+        std_itr = std_vec.erase(std_itr);
+        ft_itr = ft_vec.erase(ft_itr);
+        if ((std_itr == std_vec.end() && ft_itr != ft_vec.end())) {
+          throw std::runtime_error("return value");
+        }
+        if (std_itr != std_vec.end() && *std_itr != *ft_itr) {
+          std::cout << "std = " << *std_itr << ", ft = " << *ft_itr
+                    << std::endl;
+          throw std::runtime_error("return value");
+        }
+        for (size_t idx = 0; idx < std_vec.size(); ++idx) {
+          if (std_vec[idx] != ft_vec[idx]) {
+            std::cout << std::endl << "idx = " << idx << std::endl;
+            std::cout << "std: " << std_vec[idx] << std::endl;
+            std::cout << " ft: " << ft_vec[idx] << std::endl;
+            throw std::runtime_error("value");
+          }
+        }
+        if (std_vec.empty() != ft_vec.empty()) {
+          throw std::runtime_error("empty");
+        }
+        if (std_vec.size() != ft_vec.size()) {
+          std::cout << std::endl;
+          std::cout << "std: " << std_vec.size() << std::endl;
+          std::cout << " ft: " << ft_vec.size() << std::endl;
+          throw std::runtime_error("size");
+        }
+        if (std_vec.capacity() != ft_vec.capacity()) {
+          std::cout << std::endl;
+          std::cout << "std: " << std_vec.capacity() << std::endl;
+          std::cout << " ft: " << ft_vec.capacity() << std::endl;
+          throw std::runtime_error("capacity");
+        }
+        if (std_vec.max_size() != ft_vec.max_size()) {
+          std::cout << std::endl;
+          std::cout << "max_size(std) = " << std_vec.max_size() << std::endl;
+          std::cout << "max_size(ft)  = " << ft_vec.max_size() << std::endl;
+          throw std::runtime_error("max_size");
+        }
+      } catch (std::runtime_error& e) {
+        throw e;
+      }
+      std::cout << " => OK :)" << std::endl;
     }
   }
 
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/27 10:19:40 by dnakano           #+#    #+#             */
-/*   Updated: 2021/03/02 19:11:21 by dnakano          ###   ########.fr       */
+/*   Updated: 2021/03/02 20:05:09 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -1257,7 +1257,6 @@ class vector<bool, Allocator> {
               sizeof(storage_type) * CHAR_BIT);
     }
 
-
     const size_type storage_width = sizeof(storage_type) * CHAR_BIT;
     size_type shift_width = n % storage_width;
 
@@ -1269,15 +1268,15 @@ class vector<bool, Allocator> {
 
     // left shift
     for (size_t idx = storage_size_; idx > storageidx; --idx) {
-      if (idx  > n / storage_width) {
+      if (idx > n / storage_width) {
         storage_[idx - 1] = storage_[idx - n / storage_width - 1]
                             << shift_width;
       } else {
         storage_[idx - 1] = 0ULL;
       }
       if (idx > n / storage_width + 1 && shift_width != 0) {
-          storage_[idx - 1] |= storage_[idx - n / storage_width - 2] >>
-                              (storage_width - shift_width);
+        storage_[idx - 1] |= storage_[idx - n / storage_width - 2] >>
+                             (storage_width - shift_width);
       }
     }
 
@@ -1326,15 +1325,15 @@ class vector<bool, Allocator> {
 
     // left shift
     for (size_t idx = storage_size_; idx > storageidx; --idx) {
-      if (idx  > n / storage_width) {
+      if (idx > n / storage_width) {
         storage_[idx - 1] = storage_[idx - n / storage_width - 1]
                             << shift_width;
       } else {
         storage_[idx - 1] = 0ULL;
       }
       if (idx > n / storage_width + 1 && shift_width != 0) {
-          storage_[idx - 1] |= storage_[idx - n / storage_width - 2] >>
-                              (storage_width - shift_width);
+        storage_[idx - 1] |= storage_[idx - n / storage_width - 2] >>
+                             (storage_width - shift_width);
       }
     }
 
@@ -1351,7 +1350,28 @@ class vector<bool, Allocator> {
     }
 
     size_ += n;
-    return;
+  }
+
+  iterator erase(iterator position) {
+    const size_type storage_width = sizeof(storage_type) * CHAR_BIT;
+    size_type posidx = (*position).getIdx_();
+    size_type storageidx = posidx / (storage_width);
+    storage_type mask =
+        createMask_(posidx % (storage_width));  // 111...111000...000
+    storage_type bits_no_move = storage_[storageidx] & ~mask;
+
+    // right shift
+    for (size_t idx = storageidx; idx < storage_size_; ++idx) {
+      storage_[idx] >>= 1;
+      if (idx + 1 < storage_size_) {
+        storage_[idx] |= storage_[idx + 1] << (storage_width - 1);
+      }
+    }
+
+    storage_[storageidx] = (storage_[storageidx] & mask) | bits_no_move;
+
+    --size_;
+    return position;
   }
 
   void pop_back() { --size_; }
